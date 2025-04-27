1190d122352e422e919be794e5d4d5b2e5f45374
aleex <alee.xx1987@gmail.com>
2025-04-27 18:07:30 +0300

    new function

--- Pages/LoginPage.ts
+++ Pages/LoginPage.ts
@@ -1,5 +1,5 @@
 
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 import ApplicationURL from "../helpers/ApplicationURL";
 import { BasePage } from "./BasePage";
 
@@ -8,6 +8,7 @@ export default class LoginPage extends BasePage {
     private usernameField: Locator;
     private passwordField: Locator;
     private loginButton: Locator;
+    private signUpButton: Locator;
     // private errorMessage: Locator;
     // private default_username = process.env.STANDARD_USER as string;
     // private default_password = process.env.CORRECT_PASSWORD as string;
@@ -17,25 +18,38 @@ export default class LoginPage extends BasePage {
         super(page);
         this.usernameField = this.page.locator('input[test-id=email]');
         this.passwordField = this.page.locator('input[test-id=password]');
-        this.loginButton = this.page.locator('[data-test="login-button"]');
+        this.loginButton = this.page.getByRole('link', { name: 'Log In' });
+        this.signUpButton = this.page.getByRole('button', { name: 'Sign in', exact: true });
         //this.errorMessage = this.page.locator('[data-test="error"]')
     }
 
     public async loginToApp(){
-        //goo to url
+        //go to url
         await this.page.goto('https://cymulate.com/');
         //move to login page
-        await this.page.getByRole('link', { name: 'Log In' }).click();
-        await this.page.getByRole('heading', { name: 'Log in to your account' }).click();
+        //await this.page.getByRole('link', { name: 'Log In' }).click();
+        await this.loginButton.click();
+        //verify title of sign up page
+         expect(this.page.getByRole('heading', { name: 'Log in to your account' })).toBeVisible;
 
     }
 
     public async signIn (){
-        //Fill user name and password
-        await this.page.locator('input[test-id=email]').fill('asda');
-        await this.page.locator('input[test-id=password]').fill('asda');  
+        //Fill user name 
+        await this.usernameField.fill('user name');
+        //await this.page.locator('input[test-id=email]').fill('asda');
+
+        //expect 
+        //await expect (this.page.locator('input[test-id=email]')).toContainText('user name');
+
+        // Fill password
+        await this.passwordField.fill('password');  
+        //await this.page.locator('input[test-id=password]').fill('asda');
+
         //Press sign up button
-        await this.page.getByRole('button', { name: 'Sign in', exact: true }).click();
+        await this.signUpButton.click();
+        //await this.page.getByRole('button', { name: 'Sign in', exact: true }).click();
+
     }
 
 }
